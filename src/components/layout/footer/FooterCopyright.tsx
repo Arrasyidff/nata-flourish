0f5f6b950c5aa705fe0c464603f019fbbf0698a5
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2026-05-30 08:02:59 +0700

    fix: Update footer styling for improved visual consistency. Adjusted border thickness in FooterCopyright and reverted footer background color in Footer component for better alignment with design standards.

--- src/components/layout/footer/FooterCopyright.tsx
+++ src/components/layout/footer/FooterCopyright.tsx
@@ -2,7 +2,7 @@ export default function FooterCopyright() {
   const year = new Date().getFullYear();
 
   return (
-    <p className="text-body-3 font-normal text-green-light-active border-t border-green-dark pt-8">
+    <p className="text-body-3 font-normal text-green-light-active border-t-[0.5px] border-green-light pt-8">
       &copy; {year} NATA FLOURISH. INDONESIA.
     </p>
   );
--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -6,8 +6,7 @@ import FooterCopyright from "./FooterCopyright";
 
 export default function Footer() {
   return (
-    // <footer className="bg-green-normal-active text-green-light">
-    <footer className="bg-green-darker text-green-light">
+    <footer className="bg-green-normal-active text-green-light">
       <div className="px-10 py-16 flex flex-col gap-12">
         <div className="grid grid-cols-1 md:grid-cols-[1fr_auto_auto_auto] gap-10 md:gap-16">
           <FooterBrand />
